7eeba61495b838427f134b69aed74cb096279116
meansash <meansash@naver.com>
2023-09-01 16:40:51 +0900

    10798 verticalReading

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,9 @@
+
+=
+src/Main.java,9/2/924f089edb4af5ee45c8a9d1129cd28e2b368138
+M
+src/Advance1/wordCompare.java,f/b/fbb6524347bcfa8dec4349b4dbb755d12c3cb42b
+O
+src/Advance1/_2941_Croatia.java,a/3/a35b3805fff192fa19a8adee5ba29e02b481cd0d
+O
+src/Advance1/_1316_Checker.java,e/3/e3903b2deb291d550f99f5f8c20ee8353f8ba891
--- src/nDimensionalArrays/_10798_verticalReading.java
+++ src/nDimensionalArrays/_10798_verticalReading.java
@@ -1,10 +1,36 @@
 package nDimensionalArrays;
-
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class _10798_verticalReading {
     public static void main(String[] args) throws IOException {
-        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String[][] matrixA = new String[15][15];
 
+        for (int i = 0; i < 5; i++) {
+            String rowStr = reader.readLine();
+            for (int j = 0; j < rowStr.length(); j++) {
+                matrixA[j][i] = String.valueOf(rowStr.charAt(j));
+            }
+        }
+
+        // 행과 열을 뒤집은 배열을 만들기 위한 임시 배열
+        String[][] transposedMatrix = new String[15][15];
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                transposedMatrix[i][j] = matrixA[j][i];
+            }
+        }
+
+        // 뒤집은 배열 출력
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                if (transposedMatrix[j][i] != null) {
+                    System.out.print(transposedMatrix[j][i]);
+                }
+            }
+        }
+        System.out.println();
     }
 }
